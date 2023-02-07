2e2848e62ae936bd0c739974db3ec50dbb6eb759
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 04:43:14 +0530

    modified to player win condition

--- SnakeAndLadderProbems/Program.cs
+++ SnakeAndLadderProbems/Program.cs
@@ -8,7 +8,7 @@ namespace SnakeAndLadderProbems
         {
             Console.WriteLine("Welcome to Snake and Ladder program");
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:Player Position\n2:DieRolled\n3:CheckPosition");
+            Console.WriteLine("1:Player Position\n2:DieRolled\n3:CheckPosition\n4:win condition");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -23,6 +23,10 @@ namespace SnakeAndLadderProbems
 
                 case 3:
                     SnakeAndLadder.CheckPosition();
+                    break;
+
+                case 4:
+                    SnakeAndLadder.WinCondition();
                     break;
             }
         }
--- SnakeAndLadderProbems/SnakeAndLadder.cs
+++ SnakeAndLadderProbems/SnakeAndLadder.cs
@@ -22,7 +22,7 @@ namespace SnakeAndLadderProbems
             int dieRoll = random.Next(1, 7);
             Console.WriteLine("The random number of die: " + dieRoll);
         }
-
+        // UC3 programs
         public static void CheckPosition()
         {
             int position = 0;
@@ -45,6 +45,34 @@ namespace SnakeAndLadderProbems
                     position = position - dieRoll;
                     Console.WriteLine("Player move behind by number of die rolled " + position);
                     break;
+                }
+            }
+
+        public static void WinCondition()
+        {
+            int position = 0;
+            Random random = new Random();
+            Random random1 = new Random();
+            while (position <= 100)
+            {
+                int dieRoll = random.Next(1, 7);
+                int option = random1.Next(1, 4);
+                switch (option)
+                {
+                    case 1:
+                        position = position + dieRoll;
+                        Console.WriteLine("Player move ahead by number of Die rolled " + position);
+                        break;
+                    case 2:
+                        position = position - dieRoll;
+                        if (position < 0)
+                            position = 0;
+                        Console.WriteLine("Player move behind by number of Die rolled " + position);
+                        break;
+                    case 3:
+                        Console.WriteLine("Player remain at same position " + position);
+                        break;
+                  }
                 }
             }
         }
